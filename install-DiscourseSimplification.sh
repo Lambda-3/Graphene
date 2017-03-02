cbd5d34e0385b45898287a24f49beaa6cf26de3a
Bernhard Bermeitinger <bernhard.bermeitinger@posteo.de>
2017-03-02 13:26:17 +0100

    update install scripts

--- install-DiscourseSimplification.sh
+++ install-DiscourseSimplification.sh
@@ -2,6 +2,6 @@
 VERSION=5.0.0
 
 set -ex
-wget https://github.com/Lambda-3/DiscourseSimplification/archive/v$VERSION.tar.gz
-tar xfa v$VERSION.tar.gz
+wget https://github.com/Lambda-3/DiscourseSimplification/archive/v$VERSION.tar.gz -O DiscourseSimplification.tar.gz
+tar xfa DiscourseSimplification.tar.gz
 cd DiscourseSimplification-$VERSION && mvn install -B -DskipTest
--- install-SentenceSimplification.sh
+++ install-SentenceSimplification.sh
@@ -2,6 +2,6 @@
 VERSION=5.0.0
 
 set -ex
-wget https://github.com/Lambda-3/SentenceSimplification/archive/v$VERSION.tar.gz
-tar xfa v$VERSION.tar.gz
+wget https://github.com/Lambda-3/SentenceSimplification/archive/v$VERSION.tar.gz -O SentenceSimplification.tar.gz
+tar xfa SentenceSimplification.tar.gz
 cd SentenceSimplification-$VERSION && mvn install -B -DskipTest
