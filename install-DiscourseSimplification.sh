cec6d5be68b9f79fcedd754a45036b977b1c344d
Bernhard Bermeitinger <bernhard.bermeitinger@posteo.de>
2017-03-02 13:33:26 +0100

    skip tests when building dependencies

--- install-DiscourseSimplification.sh
+++ install-DiscourseSimplification.sh
@@ -4,4 +4,4 @@ VERSION=5.0.0
 set -ex
 wget https://github.com/Lambda-3/DiscourseSimplification/archive/v$VERSION.tar.gz -O DiscourseSimplification.tar.gz
 tar xfa DiscourseSimplification.tar.gz
-cd DiscourseSimplification-$VERSION && mvn install -B -DskipTest
+cd DiscourseSimplification-$VERSION && mvn install -B -DskipTests
--- install-SentenceSimplification.sh
+++ install-SentenceSimplification.sh
@@ -4,4 +4,4 @@ VERSION=5.0.0
 set -ex
 wget https://github.com/Lambda-3/SentenceSimplification/archive/v$VERSION.tar.gz -O SentenceSimplification.tar.gz
 tar xfa SentenceSimplification.tar.gz
-cd SentenceSimplification-$VERSION && mvn install -B -DskipTest
+cd SentenceSimplification-$VERSION && mvn install -B -DskipTests
