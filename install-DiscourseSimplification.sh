5c545646bb8670397f75d3655b3bbf20a1e4ac98
Bernhard Bermeitinger <bernhard.bermeitinger@posteo.de>
2017-05-13 17:16:33 +0200

    Delete temporary files after installing SentenceSimplification and DiscourseSimplification

--- install-DiscourseSimplification.sh
+++ install-DiscourseSimplification.sh
@@ -4,4 +4,6 @@ VERSION=6.0.0
 set -ex
 wget https://github.com/Lambda-3/DiscourseSimplification/archive/v$VERSION.tar.gz -O DiscourseSimplification.tar.gz
 tar xfa DiscourseSimplification.tar.gz
-cd DiscourseSimplification-$VERSION && mvn install -B -DskipTests
+cd DiscourseSimplification-$VERSION && mvn install -B -DskipTests && cd ..
+rm -f DiscourseSimplification.tar.gz
+rm -rf DiscourseSimplification-$VERSION 
--- install-SentenceSimplification.sh
+++ install-SentenceSimplification.sh
@@ -4,4 +4,6 @@ VERSION=5.0.0
 set -ex
 wget https://github.com/Lambda-3/SentenceSimplification/archive/v$VERSION.tar.gz -O SentenceSimplification.tar.gz
 tar xfa SentenceSimplification.tar.gz
-cd SentenceSimplification-$VERSION && mvn install -B -DskipTests
+cd SentenceSimplification-$VERSION && mvn install -B -DskipTests && cd ..
+rm -rf SentenceSimplification.tar.gz
+rm -rf SentenceSimplification-$VERSION
