acb669a81e30e51ab787212de5a6947140edfe70
Mischn <zo.matthias@gmail.com>
2018-08-08 17:28:06 +0200

    Added Request to get a list of example-sentences from Admin Resource.

--- graphene-server/src/main/java/org/lambda3/graphene/server/resources/AdminResource.java
+++ graphene-server/src/main/java/org/lambda3/graphene/server/resources/AdminResource.java
@@ -30,9 +30,17 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Arrays;
+import java.util.List;
 
 @Path("/_admin")
 public class AdminResource extends AbstractGrapheneResource {
+	private static List<String> EXAMPLE_QUERIES = Arrays.asList(
+		"Although the Treasury will announce details of the November refunding on Monday, the funding will be delayed if Congress and President Bush fail to increase the Treasury's borrowing capacity.",
+		"After I started my career as a professional photographer, I shot hundreds of people to make a living, using my DSLR camera.",
+		"After graduating from Columbia University in 1983, Barack Obama worked as a community organizer in Chicago. After that, he became a civil rights attorney.",
+		"On Monday, Asian stocks fell anew and the yen rose to session highs in the afternoon as worries about North Korea simmered, after a senior Pyongyang official said the U.S. is becoming \"more vicious and more aggressive\" under President Donald Trump."
+	);
 
 	@GET
 	@Path("version")
@@ -49,4 +57,20 @@ public class AdminResource extends AbstractGrapheneResource {
                 .entity(graphene.getVersionInfo())
                 .build();
     }
+
+	@GET
+	@Path("examples")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getExampleQueries() {
+
+		CacheControl cc = new CacheControl();
+		cc.setMaxAge(60 * 60 * 24); // seconds
+		cc.setPrivate(true);
+
+		return Response
+			.status(Response.Status.OK)
+			.cacheControl(cc)
+			.entity(EXAMPLE_QUERIES)
+			.build();
+	}
 }
