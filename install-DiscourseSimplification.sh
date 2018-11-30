d03c7936c970418537ed390b89e1befebda471f9
Juliano Efson Sales <julianoens@gmail.com>
2018-11-30 12:24:38 +0100

    solving permission in travis; install DiscourseSimplification by commit number

--- install-DiscourseSimplification.sh
+++ install-DiscourseSimplification.sh
@@ -2,7 +2,7 @@
 COMMIT=bcc9983237b71379d7024fe2a0550c20276103ad
 
 set -ex
-git clone git@github.com:Lambda-3/DiscourseSimplification.git
+git clone https://github.com/Lambda-3/DiscourseSimplification.git
 cd DiscourseSimplification
 git checkout $COMMIT
 mvn install -B -DskipTests && cd ..
